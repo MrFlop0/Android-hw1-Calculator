ab316962258f572db471633b78e2dea25741bbf7
Kaplenkov_Aleksandr <kaplenkovsasha03@gmail.com>
2022-09-12 00:37:12 +0300

    first steps

--- app/src/main/java/workflow/calculator/MainActivity.kt
+++ app/src/main/java/workflow/calculator/MainActivity.kt
@@ -4,6 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val buttons = arrayOf(
+        R.id.zero,
+        R.id.one,
+        R.id.two,
+        R.id.three,
+        R.id.four,
+        R.id.five,
+        R.id.six,
+        R.id.seven,
+        R.id.eight,
+        R.id.nine
+    )
+
+    val actions = arrayOf(
+        R.id.comma,
+        R.id.plus,
+        R.id.minus,
+        R.id.negate,
+        R.id.multiply,
+        R.id.divide,
+        R.id.clear_text,
+        R.id.clear_symbol
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
